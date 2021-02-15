8ec72d22f62c886ca4bdeb161d155eaf7b95393a
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-02-15 22:48:35 +0100

    Add green test and corresponding implementation of getting feedback

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,2 +1,25 @@
-package nl.hu.cisq1.lingo.trainer.domain;public class Feedback {
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import nl.hu.cisq1.lingo.trainer.Mark;
+
+import java.util.List;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Feedback {
+    private String guess;
+    private List<Mark> markPerLetter;
+
+    //return true when all marks in markPerLetter are correct
+    //else false
+    public Boolean isWordGuessed() {
+        if(this.markPerLetter.contains(Mark.ABSENT) || this.markPerLetter.contains(Mark.PRESENT)) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -1,4 +1,25 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import nl.hu.cisq1.lingo.trainer.Mark;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FeedbackTest {
-  
+    @Test
+    @DisplayName("Word is guessed if all letters are correct")
+    void wordIsGuessed() {
+        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        assertTrue(feedback.isWordGuessed());
+    }
+
+    @Test
+    @DisplayName("Word is not guessed if some or all letters are incorrect")
+    void wordIsNotGuessed() {
+        Feedback feedback = new Feedback("woord", List.of(Mark.ABSENT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        assertFalse(feedback.isWordGuessed());
+    }
 }
