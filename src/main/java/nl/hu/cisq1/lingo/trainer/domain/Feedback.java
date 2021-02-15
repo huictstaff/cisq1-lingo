e4337259fee847f64b9b24a099149d65fc4eefce
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-02-15 22:48:35 +0100

    Add green test and corresponding implementation of getting feedback

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,2 +1,18 @@
-package nl.hu.cisq1.lingo.trainer.domain;public class Feedback {
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import lombok.AllArgsConstructor;
+import nl.hu.cisq1.lingo.trainer.Mark;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class Feedback {
+    private String guess;
+    private List<Mark> markPerLetter;
+
+    //return true when all marks in markPerLetter are correct
+    //else false
+    public Boolean isWordGuessed() {
+        return true;
+    }
 }
