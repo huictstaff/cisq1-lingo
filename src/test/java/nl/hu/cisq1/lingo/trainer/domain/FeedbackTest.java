c496e48950435f5f9144e8f7289f516ca843c6d0
Ruben van den Brink <rubenvandenbrink@hotmail.com>
2021-02-16 22:11:26 +0100

    Add giving hints implementation and testing

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -4,8 +4,12 @@ import nl.hu.cisq1.lingo.trainer.Mark;
 import nl.hu.cisq1.lingo.trainer.exception.InvalidFeedbackException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +32,24 @@ class FeedbackTest {
     @DisplayName("InvalidFeedbackException is thrown when the length of the guess and amount of marks is not the same")
     void invalidFeedbackIsGiven() {
         assertThrows(InvalidFeedbackException.class, () -> new Feedback("woord", List.of(Mark.ABSENT)));
+    }
+
+    static Stream<Arguments> provideHintExamples() {
+        return Stream.of(
+                Arguments.of("banana", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT)),
+                Arguments.of("b.....", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT)),
+                Arguments.of("b....a", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.CORRECT)),
+                Arguments.of("b.n..a", List.of(Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.CORRECT)),
+                Arguments.of("b.n.na", List.of(Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT)),
+                Arguments.of("ban.na", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideHintExamples")
+    @DisplayName("Give correct feedback")
+    void giveFullFeedback(String expected,List<Mark> marks) {
+        Feedback feedback = new Feedback("banana", marks);
+        assertEquals(expected, feedback.giveHint());
     }
 }
