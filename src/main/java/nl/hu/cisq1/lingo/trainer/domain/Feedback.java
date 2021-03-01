e78a49c6a17fca8aa0f018bcbc88160692470672
amin <57110542+aminzammou@users.noreply.github.com>
2021-03-01 23:12:12 +0100

    hele domein omgegooid , feedback klasse maakt nu gebruik van het anwoord en een guess

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -2,20 +2,52 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Feedback {
     private final List<Mark> marks;
     private String attempt;
+    private String wordToGuess;
 
-    public Feedback(List<Mark> marks, String attempt) {
-        if (!(marks.size() == attempt.length())){
+    public Feedback(String wordToGuess, String attempt) {
+        this.wordToGuess = wordToGuess;
+        this.attempt = attempt;
+        this.marks = makeGuess(attempt,wordToGuess);
+        if (this.marks.contains(Mark.INVALID)){
             throw new InvalidFeedbackException();
-        }else{
-            this.marks = marks;
-            this.attempt = attempt;
         }
+    }
+
+//    public Feedback(List<Mark> marks, String attempt) {
+//        if (!(marks.size() == attempt.length())){
+//            throw new InvalidFeedbackException();
+//        }else{
+//            this.marks = marks;
+//            this.attempt = attempt;
+//        }
+//    }
+    public List<Mark> makeGuess(String attenpt, String wordToGuess){
+        String[] attemptLetters = attenpt.split("");
+        String[] awnserLetters = wordToGuess.split("");
+        List<Mark> markList = new ArrayList<>();
+        if(awnserLetters.length != attemptLetters.length) {
+            markList.add(Mark.INVALID);
+        }else {
+            for (int i = 0; i < awnserLetters.length; i++){
+                if (awnserLetters[i].equals(attemptLetters[i])){
+                    markList.add(Mark.CORRECT);
+                }else if (awnserLetters[i].contains(attemptLetters[i])){
+                    markList.add(Mark.PRESENT);
+                }
+                else{
+                    markList.add(Mark.ABSENT);
+                }
+            }
+        }
+
+        return markList;
     }
 
 //    public Feedback(List<Mark> marks) {
@@ -51,9 +83,10 @@ public class Feedback {
                 "marks=" + marks +
                 '}';
     }
-    public String getHint(String oldHint,String wordToGuess ){
+
+    public String getHint(String oldHint){
         String woord = "";
-        String[] letters  = wordToGuess.split("");
+        String[] letters  = this.wordToGuess.split("");
 
         if (oldHint == null){
             woord += letters[0];
@@ -71,7 +104,6 @@ public class Feedback {
             for (int i = 1; i < letters.length; i++){
                 if (!oldHintLetters[i].equals(".")){
                     woord += oldHintLetters[i];
-                    System.out.println("test");
                 }
                 else {
                     if (this.marks.get(i).equals(Mark.CORRECT)){
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -18,36 +18,36 @@ class FeedbackTest {
     @Test
     @DisplayName("word is guessed if all letters are correct")
     void wordIsGuessed() {
-        Feedback feedback = new Feedback(List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT),"woord");
+        Feedback feedback = new Feedback("woord","woord");
         assertTrue(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("word is not guessed if not all letters are correct")
     void wordIsNotGuessed() {
-        Feedback feedback = new Feedback(List.of(CORRECT, ABSENT, CORRECT, CORRECT, CORRECT), "woord");
+        Feedback feedback = new Feedback("woord", "woerd");
         assertFalse(feedback.isWordGuessed());
     }
 
-    @Test
-    @DisplayName("word is invalid if all letters are invalid")
-    void wordIsInvalid() {
-        Feedback feedback = new Feedback(List.of(INVALID, INVALID, INVALID, INVALID, INVALID),"woord");
-        assertTrue(feedback.isWordInvalid());
-    }
+//    @Test
+//    @DisplayName("word is invalid if all letters are invalid")
+//    void wordIsInvalid() {
+//        Feedback feedback = new Feedback("hhahah","woord");
+//        assertTrue(feedback.isWordInvalid());
+//    }
 
     @Test
     @DisplayName("word is valid if not all letters are marked as invalid")
     void wordIsValid() {
-        Feedback feedback = new Feedback(List.of(INVALID, ABSENT, CORRECT, CORRECT, CORRECT),"woord");
+        Feedback feedback = new Feedback("boeke","woord");
         assertFalse(feedback.isWordInvalid());
     }
 
     @Test
     @DisplayName("feedback is different if values different")
     void feedbackIsDifferent() {
-        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
-        Feedback feedbackB = new Feedback(List.of(CORRECT, INVALID),"ha");
+        Feedback feedbackA = new Feedback("ha","ha");
+        Feedback feedbackB = new Feedback("ho","ha");
 
         assertNotEquals(feedbackB, feedbackA);
     }
@@ -55,8 +55,8 @@ class FeedbackTest {
     @Test
     @DisplayName("feedback is same if values same")
     void feedbackIsSame() {
-        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
-        Feedback feedbackB = new Feedback(List.of(CORRECT, CORRECT),"ha");
+        Feedback feedbackA = new Feedback("ha","ha");
+        Feedback feedbackB = new Feedback("ha","ha");
 
         assertEquals(feedbackB, feedbackA);
     }
@@ -64,8 +64,8 @@ class FeedbackTest {
     @Test
     @DisplayName("hashcade is generated based on values")
     void hashCodeGeneration() {
-        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
-        Feedback feedbackB = new Feedback(List.of(CORRECT, CORRECT),"ha");
+        Feedback feedbackA = new Feedback("ha","ha");
+        Feedback feedbackB = new Feedback("ha","ha");
 
         assertEquals(feedbackB.hashCode(), feedbackA.hashCode());
     }
@@ -73,7 +73,7 @@ class FeedbackTest {
     @Test
     @DisplayName("toString contains class name")
     void convertedToString() {
-        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
+        Feedback feedbackA = new Feedback("ha","ha");
         assertTrue(feedbackA.toString().contains("Feedback"));
     }
 //    @Test
@@ -90,7 +90,7 @@ class FeedbackTest {
     void InvalidLenght(){
         assertThrows(
                 InvalidFeedbackException.class,
-                () -> new Feedback(List.of(Mark.CORRECT), "woord")
+                () -> new Feedback("kat", "woord")
         );
     }
 //    @ParameterizedTest
@@ -111,17 +111,141 @@ class FeedbackTest {
     @ParameterizedTest
     @DisplayName("the user guessed some letters of the word")
     @MethodSource("provideHints")
-    void giveHint(String testword, String wordToGues){
-        Feedback feedback = new Feedback(List.of(ABSENT, ABSENT, ABSENT,ABSENT),"hond");
-        assertEquals(testword,feedback.getHint(".o..", wordToGues));
+    void giveHint(String wordToGues, String attempt, String oldHint, String expected){
+        Feedback feedback = new Feedback(wordToGues, attempt);
+        assertEquals(expected,feedback.getHint(oldHint));
     }
 
     private static Stream<Arguments> provideHints(){
         return Stream.of(
-//                Arguments.of("h..l.","h..l."),
-//                Arguments.of("ha.l.","h..l."),
-                Arguments.of("hond","hond")
+                Arguments.of("baard","brood",null,"b...d"),
+                Arguments.of("baard","blaar","b...d","b.a.d"),
+                Arguments.of("baard","beren","b.a.d","b.a.d"),
+                Arguments.of("baard","baard","b.a.d","baard")
         );
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    @DisplayName("word is guessed if all letters are correct")
+//    void wordIsGuessed() {
+//        Feedback feedback = new Feedback(List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT),"woord");
+//        assertTrue(feedback.isWordGuessed());
+//    }
+//
+//    @Test
+//    @DisplayName("word is not guessed if not all letters are correct")
+//    void wordIsNotGuessed() {
+//        Feedback feedback = new Feedback(List.of(CORRECT, ABSENT, CORRECT, CORRECT, CORRECT), "woord");
+//        assertFalse(feedback.isWordGuessed());
+//    }
+//
+//    @Test
+//    @DisplayName("word is invalid if all letters are invalid")
+//    void wordIsInvalid() {
+//        Feedback feedback = new Feedback(List.of(INVALID, INVALID, INVALID, INVALID, INVALID),"woord");
+//        assertTrue(feedback.isWordInvalid());
+//    }
+//
+//    @Test
+//    @DisplayName("word is valid if not all letters are marked as invalid")
+//    void wordIsValid() {
+//        Feedback feedback = new Feedback(List.of(INVALID, ABSENT, CORRECT, CORRECT, CORRECT),"woord");
+//        assertFalse(feedback.isWordInvalid());
+//    }
+//
+//    @Test
+//    @DisplayName("feedback is different if values different")
+//    void feedbackIsDifferent() {
+//        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//        Feedback feedbackB = new Feedback(List.of(CORRECT, INVALID),"ha");
+//
+//        assertNotEquals(feedbackB, feedbackA);
+//    }
+//
+//    @Test
+//    @DisplayName("feedback is same if values same")
+//    void feedbackIsSame() {
+//        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//        Feedback feedbackB = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//
+//        assertEquals(feedbackB, feedbackA);
+//    }
+//
+//    @Test
+//    @DisplayName("hashcade is generated based on values")
+//    void hashCodeGeneration() {
+//        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//        Feedback feedbackB = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//
+//        assertEquals(feedbackB.hashCode(), feedbackA.hashCode());
+//    }
+//
+//    @Test
+//    @DisplayName("toString contains class name")
+//    void convertedToString() {
+//        Feedback feedbackA = new Feedback(List.of(CORRECT, CORRECT),"ha");
+//        assertTrue(feedbackA.toString().contains("Feedback"));
+//    }
+////    @Test
+////    @DisplayName("toString contains class name")
+////    void InvalidLenght() {
+////        assertThrows(
+////                InvalidFeedbackException.class,
+////                () -> new Feedback(List.of(CORRECT, CORRECT),"banaan")
+////        );
+////    }
+//
+//    @Test
+//    @DisplayName("Feedback does not have the same size as the attempt")
+//    void InvalidLenght(){
+//        assertThrows(
+//                InvalidFeedbackException.class,
+//                () -> new Feedback(List.of(Mark.CORRECT), "woord")
+//        );
+//    }
+////    @ParameterizedTest
+////    @DisplayName("the user guessed some letters of the word")
+////    @MethodSource("provideHints")
+////    void giveHint(String hint, String testHint){
+////        assertEquals(hint,testHint);
+////    }
+////
+////    private static Stream<Arguments> provideHints(){
+////        return Stream.of(
+////                Arguments.of("h..l.","h..l."),
+////                Arguments.of("ha.l.","h..l."),
+////                Arguments.of("h..lo","h..lo")
+////        );
+////    }
+//
+//    @ParameterizedTest
+//    @DisplayName("the user guessed some letters of the word")
+//    @MethodSource("provideHints")
+//    void giveHint(String testword, String wordToGues){
+//        Feedback feedback = new Feedback(List.of(ABSENT, ABSENT, ABSENT,ABSENT),"hond");
+//        assertEquals(testword,feedback.getHint(".o..", wordToGues));
+//    }
+//
+//    private static Stream<Arguments> provideHints(){
+//        return Stream.of(
+////                Arguments.of("h..l.","h..l."),
+////                Arguments.of("ha.l.","h..l."),
+//                Arguments.of("hond","hond")
+//        );
+//    }
 
 }
