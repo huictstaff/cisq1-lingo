1f16ff2636c0ad765fcf55d27ce2afb38b99c34b
EvripidouNL <evripidounl@gmail.com>
2021-03-01 23:52:18 +0100

    Added GameTest. Score based on attempts and add one round to game

--- src/main/java/nl/hu/cisq1/lingo/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/domain/Game.java
@@ -1,16 +1,20 @@
 package nl.hu.cisq1.lingo.domain;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
+import nl.hu.cisq1.lingo.domain.exception.RoundAttemptLimitException;
 import nl.hu.cisq1.lingo.domain.exception.RoundDoesNotBelongToGameException;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.List;
 
 @EqualsAndHashCode
 @ToString
+@Getter
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,13 +28,23 @@ public class Game {
         this.rounds = rounds;
     }
 
-    public double addScore(Round round) {
+    public void addScore(Round round, int attempts) {
         if (!this.rounds.contains(round)) {
             throw new RoundDoesNotBelongToGameException();
         }
 
-        this.score += 5 * (5 - round.getAttempt()) + 5;
+        if(attempts > 5) {
+            throw new RoundAttemptLimitException();
+        }
 
-        return score;
+        this.score += 5 * (5 - attempts) + 5;
+    }
+
+    public void newRound(Word word) {
+        Round round = new Round(this.rounds.size() +1, word, new ArrayList<>());
+
+        round.startRound();
+
+        this.rounds.add(round);
     }
 }
---
+++ src/test/java/nl/hu/cisq1/lingo/domain/GameTest.java
@@ -0,0 +1,49 @@
+package nl.hu.cisq1.lingo.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    @ParameterizedTest(name = "Test #{index} | {0} | {1} | {2} " )
+    @DisplayName("Score based on attempts in round")
+    @MethodSource("provideAttemptExamples")
+    void addScore(String word, int attempts, int score) {
+        Game game = new Game(1L,  0, new ArrayList<>());
+
+        game.newRound(new Word(word));
+
+        Round round = game.getRounds().get(game.getRounds().size() -1);
+
+        game.addScore(round, attempts);
+
+        assertEquals(score, game.getScore());
+    }
+
+    private static Stream<Arguments> provideAttemptExamples() {
+        return Stream.of(
+                Arguments.of("woord", 1, 25),
+                Arguments.of("woord", 2, 20),
+                Arguments.of("woord", 3, 15),
+                Arguments.of("woord", 4, 10),
+                Arguments.of("woord", 5, 5)
+        );
+    }
+
+    @Test
+    @DisplayName("Add one new rounds to game")
+    void newRound() {
+        Game game = new Game(1L,  0, new ArrayList<>());
+
+        game.newRound(new Word("woord"));
+
+        assertEquals(1, game.getRounds().size());
+    }
+}
