b19d13f3b7b6358614a204a96f8618bce5b10495
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-03-04 14:34:22 +0100

    Added feedback for guesses and tests for that Feedback

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,46 +1,24 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
+import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 @ToString
 public class Feedback {
-    private String word;
-    private List<Mark> marks = new ArrayList<>();
+    private String guess;
+    private List<Mark> marks;
 
-    public Feedback(String word, List<Mark> marks) {
-        if (marks.size() != word.length()) {
-            throw new InvalidFeedbackException(String.valueOf(word.length()));
-        }
-        this.word = word;
-        this.marks = marks;
+    public boolean wordIsGuessed() {
+        if (marks.size() != guess.length()) return false;
+        return marks.stream().allMatch(mark -> mark == Mark.CORRECT);
     }
 
-    public Feedback(String guess, String actualWord) {
-        if (guess.length() != actualWord.length()) {
-            throw new InvalidFeedbackException(String.valueOf(guess.length()));
-        }
-        this.word = guess;
-        for (int index = 0; index < word.length(); index++) {
-            char guessedLetter = word.charAt(index);
-            char actualLetter = actualWord.charAt(index);
-            if (guessedLetter == actualLetter) {
-                this.marks.add(Mark.CORRECT);
-            } else if (actualWord.indexOf(guessedLetter) != -1) {
-                this.marks.add(Mark.PRESENT);
-            } else this.marks.add(Mark.ABSENT);
-        }
-    }
-
-    boolean isWordGuessed() {
-        if (marks.isEmpty()) return false;
-        return this.marks.stream().allMatch(mark -> mark == Mark.CORRECT);
+    public boolean guessIsValid() {
+        return marks.size() == guess.length();
     }
 }
