450aa16ccb10e9aef671d90ae2c248b3df682fa8
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-03-11 01:48:54 +0100

    exceptions toevoegen

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/exception/InvalidAttemptException.java
@@ -0,0 +1,7 @@
+package nl.hu.cisq1.lingo.trainer.exception;
+
+public class InvalidAttemptException extends RuntimeException{
+    public InvalidAttemptException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/exception/InvalidFeedbackException.java
@@ -0,0 +1,7 @@
+package nl.hu.cisq1.lingo.trainer.exception;
+
+public class InvalidFeedbackException extends RuntimeException {
+    public InvalidFeedbackException(String message){
+        super(message);
+    }
+}
