ee75e7c0c500bb06f2be03cf6b46ca5fe4357e4e
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-03-12 11:35:26 +0100

    Toevoegen validatie, sonarcloud, jacoco en PiTest

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Validator.java
@@ -0,0 +1,41 @@
+package nl.hu.cisq1.lingo.trainer.domain;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@AllArgsConstructor
+public class Validator {
+    private final String guess;
+    private final String word;
+
+    public List<Mark> validate() {
+        HashMap<Character, Integer> availableChars = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (letter != guess.charAt(i)) {
+                int count = availableChars.getOrDefault(letter, 0);
+                availableChars.put(letter, count + 1);
+            }
+        }
+        List<Mark> marks = new ArrayList<>();
+        for (int i = 0; i < guess.length(); i++) {
+            char letter = guess.charAt(i);
+            char actualLetter = word.charAt(i);
+            if (letter == actualLetter) {
+                marks.add(Mark.CORRECT);
+            } else if (this.word.indexOf(letter) < 0) {
+                marks.add(Mark.ABSENT);
+            } else {
+                int count = availableChars.getOrDefault(letter, 0);
+                if (count > 0) {
+                    marks.add(Mark.PRESENT);
+                    availableChars.put(letter, count - 1);
+                } else marks.add(Mark.ABSENT);
+            }
+        }
+        return marks;
+    }
+}
