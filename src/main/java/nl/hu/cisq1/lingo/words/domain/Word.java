0d4ddd5b0d1d3eade5be760b3a7e7f18eb5033c4
Jan Rietveld <jan.rietveld@student.hu.nl>
2021-03-18 07:54:35 +0100

    Word fix

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
@@ -3,10 +3,9 @@ package nl.hu.cisq1.lingo.words.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity(name = "words")
-public class Word implements Serializable {
+public class Word {
     @Id
     @Column(name = "word")
     private String value;
