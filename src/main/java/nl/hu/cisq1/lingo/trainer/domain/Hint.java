918c073dc6d85d48a52039433fe920742e656cd7
JustMilan <milandol321@gmail.com>
2021-03-24 20:47:57 +0100

    Add giveHint method

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Hint.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Hint.java
@@ -1,14 +1,16 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidListSizeException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Hint {
     private List<Character> hint;
 
-     public Hint(List<Character> hint) {
-         this.hint = hint;
-     }
+    public Hint(List<Character> hint) {
+        this.hint = hint;
+    }
 
     public void setHint(List<Character> hint) {
         this.hint = hint;
@@ -16,5 +18,34 @@ public class Hint {
 
     public List<Character> getHint() {
         return hint;
+    }
+
+    public List<Character> giveHint(List<Guess> guesses, List<Feedback> feedbackList, String word) {
+        ArrayList<Character> charList = new ArrayList<>();
+        if (feedbackList.size() != guesses.size()) {
+            throw new InvalidListSizeException();
+        }
+
+        for (Guess guess : guesses) {
+            for (int i = 0; i <= guess.getGuess().length(); i++) {
+                List<Mark> marksList = feedbackList.get(i).getMarks();
+                for (Mark mark : marksList) {
+                    switch (mark) {
+                        case CORRECT:
+                            charList.set(i, word.charAt(i));
+                        case PRESENT:
+                            if (charList.get(i) != word.charAt(i)) {
+                                charList.set(i, '+');
+                            }
+                        case ABSENT:
+                            if (charList.get(i) != word.charAt(i)) {
+                                charList.set(i, '-');
+                            }
+                    }
+                }
+            }
+        }
+        this.hint = charList;
+        return charList;
     }
 }
