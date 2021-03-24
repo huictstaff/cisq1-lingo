e297b93076fd1fbcaf2a7e39a1b89d4b1fd0bff3
JustMilan <milandol321@gmail.com>
2021-03-24 20:52:03 +0100

    Add game class logic

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -1,9 +1,63 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
-import nl.hu.cisq1.lingo.words.application.WordService;
+import nl.hu.cisq1.lingo.trainer.domain.exception.ActiveRoundException;
+import nl.hu.cisq1.lingo.trainer.domain.exception.GameLostException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
-    public void startGame(String word) {
-        Round round = new Round(word);
+    private int points;
+    private GameState gameState;
+    private long id;
+    private Round round;
+    private final List<Round> rounds;
+
+    public Game() {
+        this.rounds = new ArrayList<>();
+        this.gameState = GameState.CONTINUE;
+    }
+
+    public Game(int id) {
+        this.id = id;
+        this.rounds = new ArrayList<>();
+        this.gameState = GameState.CONTINUE;
+    }
+
+    public void newRound(String word) {
+        if (this.round != null) {
+            throw new ActiveRoundException();
+        } else if (this.gameState.equals(GameState.LOST)) {
+            throw new GameLostException();
+        }
+        this.rounds.add(new Round(word));
+    }
+
+    public void makeGuess(String guess) {
+        this.round.makeGuess(guess);
+    }
+
+    public GameState checkGameState() {
+        return this.round.getGameState();
+    }
+
+    public void calculateScore() {
+        if (this.gameState == GameState.WON) {
+            this.points += 5 * (5 - this.round.getGuesses().size()) + 5;
+        }
+    }
+
+    public int provideNextLenghtWord() {
+        return switch (this.round.getWord().length()) {
+            case 5 -> 6;
+            case 6 -> 7;
+            default -> 5;
+        };
+    }
+
+    public void endRound() {
+        this.rounds.add(this.round);
+        calculateScore();
+        this.gameState = this.checkGameState();
     }
 }
