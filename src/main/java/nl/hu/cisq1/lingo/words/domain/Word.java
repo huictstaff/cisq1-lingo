df4ce861edc6d8d3585c277509b95bb08c4a9235
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-03-29 10:55:26 +0200

    Updated word to have equals

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
@@ -3,6 +3,7 @@ package nl.hu.cisq1.lingo.words.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity(name = "words")
 public class Word {
@@ -23,5 +24,13 @@ public class Word {
 
     public Integer getLength() {
         return length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word)) return false;
+        Word word = (Word) o;
+        return Objects.equals(getValue(), word.getValue()) && Objects.equals(getLength(), word.getLength());
     }
 }
