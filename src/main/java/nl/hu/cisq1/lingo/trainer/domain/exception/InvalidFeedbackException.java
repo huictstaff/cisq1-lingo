2e87ac2e335e6039de0d6932ed6d58658e3be800
Milan Dol <milandol321@gmail.com>
2021-04-03 21:56:50 +0200

    Remove unused Exception

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidFeedbackException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.cisq1.lingo.trainer.domain.exception;
-
-public class InvalidFeedbackException extends RuntimeException {
-    public InvalidFeedbackException(String guess, int requiredWordLength, int round) {
-        super(String.format("Guess %s with a length of %d doens't match the required length of %d for round %d", guess, guess.length(), requiredWordLength, round));
-    }
-}
