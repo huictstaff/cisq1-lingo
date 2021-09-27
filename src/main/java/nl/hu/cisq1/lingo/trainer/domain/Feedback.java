30fcf1124d1935bcbfb651578940d51fa7d11ba6
Fong Suen <fong.suen@student.hu.nl>
2021-09-27 15:38:15 +0200

    Deelopdracht 3 stap 2: eerste draft van giveHint (is nog fout, moet nog aanpassen om de vorige hint toe te werken)

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,5 +1,6 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
@@ -7,10 +8,37 @@ import java.util.function.Predicate;
 public class Feedback {
     private String attempt;
     private List<Mark> markList;
+    private List<Character> hintList = new ArrayList<>();
+    private List<Character> previousHintList = new ArrayList<>();
+
 
     public Feedback(String attempt, List<Mark> markList) {
         this.attempt = attempt;
         this.markList = markList;
+        for (int index=0 ; index<attempt.length(); index++  ) {
+            String empty = "-";
+            char toAdd = empty.charAt(0);
+            hintList.add(toAdd);
+        }
+    }
+
+    public List<Character> giveHint() {
+        int index = 0;
+        for (Mark mark: markList){
+            System.out.println(hintList);
+            System.out.println(mark);
+            System.out.println(attempt.charAt(index));
+            System.out.println(index);
+
+            if (mark==Mark.CORRECT){
+
+                char toAdd = attempt.charAt(index);
+                hintList.set(index,toAdd);
+            }
+            index+=1;
+        }
+        System.out.println(hintList.toString());
+        return hintList;
     }
 
     public boolean isWordGuessed() {
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -3,6 +3,7 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,21 +24,34 @@ class FeedbackTest {
     }
 
     @Test
-    @DisplayName("word is not guessed because all the letters are invallid")
+    @DisplayName("word is not guessed because all the letters are invalid")
     void guessIsInvalid(){
         Feedback feedback = new Feedback("woord", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID));
         assertFalse(feedback.isWordGuessed());
     }
 
     @Test
-    @DisplayName("word is not guessed because all the letters are invallid")
+    @DisplayName("word is not guessed because all the letters are invalid")
     void guessIsNotInvalid(){
         Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
         assertTrue(feedback.isWordGuessed());
     }
 
+    @Test
+    @DisplayName("hint given matches the hint expected")
+    void hintIsCorrect(){
+        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT));
+
+        String expectedHint = "woo-d";
+        List<Character> expectedHintList = new ArrayList<>();
+
+        for (char ch: expectedHint.toCharArray()) {
+            expectedHintList.add(ch);
+        }
+        assertEquals(expectedHintList,feedback.giveHint());
 
 
+    }
 
 
 }
