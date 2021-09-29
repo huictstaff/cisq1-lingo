0b27ddd200da81d65da9180f6e3d317687e19620
Fong Suen <fong.suen@student.hu.nl>
2021-09-29 22:31:19 +0200

    Deelopdracht 3 step 2: Added ParameterizedTest and removed old giveHint tests

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -2,13 +2,27 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedbackTest {
+    public static Stream<Arguments> hintTest(){
+        return Stream.of(
+          Arguments.of(new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT)), "wo---", "woo-d"),
+          Arguments.of(new Feedback("woord", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.CORRECT)), "w----", "w---d"),
+          Arguments.of(new Feedback("woord", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.CORRECT)), "", "w---d"),
+          Arguments.of(new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT)), "", "woord"),
+          Arguments.of(new Feedback("woord", List.of(Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT)), "woor-", "woor-")
+
+        );
+    }
     @Test
     @DisplayName("word is guessed if all letters are correct")
     void wordIsGuessed(){
@@ -36,40 +50,11 @@ class FeedbackTest {
         Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
         assertTrue(feedback.isWordGuessed());
     }
-
-
-
-    @Test
-    @DisplayName("hint given matches the hint expected")
-    void hintWithFormerHint(){
-        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.CORRECT));
-
-        String expectedHint = "woo-d";
-        String formerHint = "wo---";
-
+    
+    @ParameterizedTest
+    @MethodSource({"hintTest"})
+    @DisplayName("Hint given depends on the attempted word andprevious hint")
+    void giveHint(Feedback feedback, String formerHint, String expectedHint){
         assertEquals(expectedHint,feedback.giveHint(formerHint));
     }
-
-    @Test
-    @DisplayName("hint given matches the hint expected")
-    void hintWithoutFormerHint(){
-        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.ABSENT));
-
-        String expectedHint = "woo--";
-        String formerHint = "";
-
-        assertEquals(expectedHint,feedback.giveHint(formerHint));
-    }
-
-    @Test
-    @DisplayName("hint given matches the hint expected")
-    void hintButNothingInTheAttemptWasCorrect(){
-        Feedback feedback = new Feedback("woord", List.of(Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT));
-
-        String expectedHint = "jo---";
-        String formerHint = "jo---";
-
-        assertEquals(expectedHint,feedback.giveHint(formerHint));
-    }
-
 }
