2a57d97282f087ffabe3d5096cf2a98e866fb00c
Alex Rothuis <alex.rothuis@gmail.com>
2022-01-27 11:21:45 +0100

    Use PostgreSQL test database for more deterministic integration tests
    Use H2 for our CI setup when Spring profile ci is active

---
+++ src/main/java/nl/hu/cisq1/lingo/words/presentation/WordResult.java
@@ -0,0 +1,13 @@
+package nl.hu.cisq1.lingo.words.presentation;
+
+public class WordResult {
+    private String word;
+
+    public WordResult(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
